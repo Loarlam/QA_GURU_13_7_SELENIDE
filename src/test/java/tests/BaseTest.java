b4703a0d2e6a27933f494be8f001fc3bc08ad89d
Loarlam <Loarlam@ya.ru>
2022-06-18 21:09:08 +0300

    Начало разработки теста.

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,22 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.*;
+import static com.codeborne.selenide.Selenide.open;
+
+public class BaseTest {
+
+    @BeforeAll
+    static void beforeAllTests() {
+        open("https://github.com/selenide");
+        Configuration.browserSize = "1920x1080";
+        Configuration.browserPosition = "0x0";
+    }
+
+    @AfterAll
+    static void afterAllTest() throws InterruptedException {
+        Configuration.holdBrowserOpen = true;
+        Thread.sleep(2000);
+        Configuration.holdBrowserOpen = false;
+    }
+}
